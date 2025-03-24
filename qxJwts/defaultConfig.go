7ff03b319d66656e0d12aacf85c3caea47688f96
howard <howard.huang@itech99.com>
2025-03-24 22:35:40 +0800

    feat: 发布v0.0.33

---
+++ qxJwts/defaultConfig.go
@@ -0,0 +1,8 @@
+package qxJwts
+
+type QxJwtDefaultConfigWithKms struct {
+	CertName          string `json:",default=default"`
+	CertPublicKeyPath string `json:",default=etc/jwt.public.crt"`
+	SignMethod        string `json:",default=ES384"`
+	OnlineExp         int    `json:",default=300"`
+}
