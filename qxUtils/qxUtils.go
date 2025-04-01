9ca1b5685306a1138ef709ed0fa940cbf79ac74f
howard <howard.huang@itech99.com>
2025-04-01 11:05:49 +0800

    feat: 发布v0.0.51

--- qxUtils/qxUtils.go
+++ qxUtils/qxUtils.go
@@ -2,6 +2,38 @@ package qxUtils
 
 import "github.com/Technology-99/qxLib/qxCrypto"
 
+type ICPRecord struct {
+	IsShow bool   `json:"isShow"`
+	Icon   string `json:"icon"`
+	Href   string `json:"href"`
+	Text   string `json:"text"`
+}
+
+func (r *ICPRecord) Default() ICPRecord {
+	return ICPRecord{
+		IsShow: false,
+		Icon:   "https://corecdn.csvw88.com/statics/gov/icp.gif",
+		Href:   "https://beian.miit.gov.cn/",
+		Text:   "XICP备1996091901号-x",
+	}
+}
+
+type MpsRecord struct {
+	IsShow bool   `json:"isShow"`
+	Icon   string `json:"icon"`
+	Href   string `json:"href"`
+	Text   string `json:"text"`
+}
+
+func (r *MpsRecord) Default() MpsRecord {
+	return MpsRecord{
+		IsShow: false,
+		Icon:   "https://corecdn.csvw88.com/statics/gov/mps.png",
+		Href:   "https://beian.miit.gov.cn/",
+		Text:   "X公网安备 00000000000000号",
+	}
+}
+
 func GenPassword(realPwd string) (encryptPwdBase, key, salt string) {
 	key = qxCrypto.RandStr(32)
 	// 生成盐值
