3fb4a777c0f5d65241ccef34778009777b0c056c
howard <howard.huang@itech99.com>
2025-04-27 17:33:37 +0800

    feat: 发布v0.0.84

--- qxCodes/code.go
+++ qxCodes/code.go
@@ -229,6 +229,10 @@ const (
 	BsStatusOauth2CodeExpired                   int32 = 40059 // 语义: oauth2: code已过期
 	BsStatusOauth2CodeRevoked                   int32 = 40060 // 语义: oauth2: code已撤销
 	BsStatusOauth2CodeUsed                      int32 = 40061 // 语义: oauth2: code已被使用
+	BsStatusOauth2ClientIdNotMatch              int32 = 40062 // 语义: 错误的client_id
+	BsStatusOauth2RedirectUriNotMatch           int32 = 40063 // 语义: 错误的redirect_uri
+	BsStatusOauth2CodeVerifierIsEmpty           int32 = 40064 // 语义: code_verifier不能为空
+	BsStatusOauth2CodeVerifierNotMatch          int32 = 40065 // 语义: 错误的code_verifier
 	BsStatusInternalServerError                 int32 = 50000 // 语义: 服务器内部错误，无法完成请求
 	BsStatusNotImplemented                      int32 = 50001 // 语义: 服务器不支持请求的功能，无法完成请求
 	BsStatusBadGateway                          int32 = 50002 // 语义: 作为网关或者代理工作的服务器尝试执行请求时，从远程服务器接收到了一个无效的响应
