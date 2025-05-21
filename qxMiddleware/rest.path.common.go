c72f36274971264474c385ea84560ca01c21a4e0
howard <howard.huang@itech99.com>
2025-05-21 16:53:35 +0800

    feat: 发布v0.1.9

--- qxMiddleware/rest.path.common.go
+++ qxMiddleware/rest.path.common.go
@@ -2,14 +2,16 @@ package qxMiddleware
 
 import (
 	"context"
+	"github.com/Technology-99/qxLib/qxCodes"
 	"github.com/Technology-99/qxLib/qxCommonHeader"
+	"github.com/Technology-99/qxLib/qxErrors"
+	"github.com/Technology-99/qxLib/qxRes"
 	"github.com/google/uuid"
 	"github.com/zeromicro/go-zero/core/logc"
 	"github.com/zeromicro/go-zero/core/logx"
 	"github.com/zeromicro/go-zero/rest/httpx"
+	"net"
 	"net/http"
-	"regexp"
-	"strings"
 	"time"
 )
 
@@ -27,34 +29,44 @@ func (m *PathHttpInterceptorMiddleware) Handle(next http.HandlerFunc) http.Handl
 		ctx = context.WithValue(ctx, CtxRequestURI, r.RequestURI)
 		ctx = context.WithValue(ctx, CtxStartTime, startTime.UnixMilli())
 		fullAddr := httpx.GetRemoteAddr(r)
-		// 定义正则表达式
-		regex := `^\[?([a-fA-F0-9:.%]+)\]?(?::([0-9]+))?$`
-		re := regexp.MustCompile(regex)
-		// 去除首尾空格
-		fullAddr = strings.TrimSpace(fullAddr)
-		// 匹配输入字符串
-		matches := re.FindStringSubmatch(fullAddr)
-		// 匹配输入字符串
-		if len(matches) != 3 {
-			fullAddrAndPort := strings.Split(fullAddr, ":")
-			if len(fullAddrAndPort) == 1 {
-				ctx = context.WithValue(ctx, CtxClientIp, fullAddrAndPort[0])
-				ctx = context.WithValue(ctx, CtxClientPort, "")
-			} else if len(fullAddrAndPort) == 2 {
-				ctx = context.WithValue(ctx, CtxClientIp, fullAddrAndPort[0])
-				ctx = context.WithValue(ctx, CtxClientPort, fullAddrAndPort[1])
-			} else {
-				logx.Errorf("fullAddr: %s, fullAddrAndPort: %v", fullAddr, fullAddrAndPort)
-			}
-		} else {
-			clientIp := matches[1]
-			clientPort := matches[2]
-			ctx = context.WithValue(ctx, CtxClientPort, clientPort)
-			if matches[1] == "::1" {
-				clientIp = "127.0.0.1"
-			}
-			ctx = context.WithValue(ctx, CtxClientIp, clientIp)
+		ip, port, err := net.SplitHostPort(fullAddr)
+		if err != nil {
+			qxRes.JsonBaseResponseCtx(r.Context(), w, r, nil, qxErrors.Quick(qxCodes.StatusNotImplemented, qxCodes.LangZhCN))
+			return
 		}
+		logx.Infof("ip: %s, port: %s", ip, port)
+
+		ctx = context.WithValue(ctx, CtxClientIp, ip)
+		ctx = context.WithValue(ctx, CtxClientPort, port)
+
+		//// 定义正则表达式
+		//regex := `^\[?([a-fA-F0-9:.%]+)\]?(?::([0-9]+))?$`
+		//re := regexp.MustCompile(regex)
+		//// 去除首尾空格
+		//fullAddr = strings.TrimSpace(fullAddr)
+		//// 匹配输入字符串
+		//matches := re.FindStringSubmatch(fullAddr)
+		//// 匹配输入字符串
+		//if len(matches) != 3 {
+		//	fullAddrAndPort := strings.Split(fullAddr, ":")
+		//	if len(fullAddrAndPort) == 1 {
+		//		ctx = context.WithValue(ctx, CtxClientIp, fullAddrAndPort[0])
+		//		ctx = context.WithValue(ctx, CtxClientPort, "")
+		//	} else if len(fullAddrAndPort) == 2 {
+		//		ctx = context.WithValue(ctx, CtxClientIp, fullAddrAndPort[0])
+		//		ctx = context.WithValue(ctx, CtxClientPort, fullAddrAndPort[1])
+		//	} else {
+		//		logx.Errorf("fullAddr: %s, fullAddrAndPort: %v", fullAddr, fullAddrAndPort)
+		//	}
+		//} else {
+		//	clientIp := matches[1]
+		//	clientPort := matches[2]
+		//	ctx = context.WithValue(ctx, CtxClientPort, clientPort)
+		//	if matches[1] == "::1" {
+		//		clientIp = "127.0.0.1"
+		//	}
+		//	ctx = context.WithValue(ctx, CtxClientIp, clientIp)
+		//}
 
 		requestID := uuid.NewString()
 		ctx = context.WithValue(ctx, CtxRequestID, requestID)
