006843f6745a181e2841a1ddd21832f619d0c907
howard <howard.huang@itech99.com>
2025-05-22 19:47:45 +0800

    feat: 发布v0.1.16

--- qxMiddleware/rest.path.common.go
+++ qxMiddleware/rest.path.common.go
@@ -2,7 +2,7 @@ package qxMiddleware
 
 import (
 	"context"
-	"fmt"
+	"errors"
 	"github.com/Technology-99/qxLib/qxCommonHeader"
 	"github.com/google/uuid"
 	"github.com/zeromicro/go-zero/core/logc"
@@ -28,27 +28,19 @@ func (m *PathHttpInterceptorMiddleware) Handle(next http.HandlerFunc) http.Handl
 		ctx = context.WithValue(ctx, CtxRequestURI, r.RequestURI)
 		ctx = context.WithValue(ctx, CtxStartTime, startTime.UnixMilli())
 		fullAddr := httpx.GetRemoteAddr(r)
+		logc.Infof(ctx, "fullAddr:%v", fullAddr)
 		ips := strings.Split(fullAddr, ",")
 		realAddr := ips[0]
-		ip := ""
-		port := ""
-		var err error
-		if ipType(realAddr) == "IPv4" {
-			if strings.Contains(realAddr, ":") {
-				// note: 带端口号的ip
-				ip, port, err = net.SplitHostPort(realAddr)
-			} else {
-				// note: 不带端口号的ip
-				ip = realAddr
-				port = ""
-			}
-			ctx = context.WithValue(ctx, CtxClientIp, ip)
-			ctx = context.WithValue(ctx, CtxClientPort, port)
-		} else {
-			ip = fmt.Sprintf("%s", net.ParseIP(realAddr))
-			ctx = context.WithValue(ctx, CtxClientIp, ip)
-			ctx = context.WithValue(ctx, CtxClientPort, "")
+		ip, port, ipType, err := returnIpAndPort(realAddr)
+		if err != nil {
+			logc.Infof(ctx, "unknown ip format: %s", err)
+			http.Error(w, "unknown ip format", http.StatusNotImplemented)
+			return
 		}
+		logc.Infof(ctx, "realAddr: %s, ip: %s, port: %s, ipType: %s, err: %s", realAddr, ip, port, ipType, err)
+
+		ctx = context.WithValue(ctx, CtxClientIp, ip)
+		ctx = context.WithValue(ctx, CtxClientPort, port)
 		logc.Infof(ctx, "IP: %s, Port: %s", ip, port)
 		if err != nil {
 			logx.Infof("解析ip报错: %s", err)
@@ -72,13 +64,56 @@ func (m *PathHttpInterceptorMiddleware) Handle(next http.HandlerFunc) http.Handl
 	}
 }
 
-func ipType(ipStr string) string {
-	ip := net.ParseIP(ipStr)
-	if ip == nil {
-		return "invalid"
+func returnIpAndPort(ipStr string) (ip, port, ipType string, err error) {
+	ip = ""
+	port = ""
+	ipType = "IPv4"
+	// note: 先判断是否是 IPv6
+	if strings.Count(ipStr, ":") > 1 {
+		if strings.Contains(ipStr, "[") {
+			// note: ipv6带端口
+			host, p, err := net.SplitHostPort(ipStr)
+			if err != nil {
+				logx.Errorf("❌ 无效的带端口 IPv6:", err)
+				return ip, port, ipType, err
+			}
+			ip = host
+			port = p
+		} else {
+			ip = ipStr
+			port = ""
+		}
+	} else {
+		// note: 判断不是ipv4
+		if strings.Contains(ipStr, ":") {
+			host, p, err := net.SplitHostPort(ipStr)
+			if err != nil {
+				logx.Errorf("❌ 无效的带端口 IPv6:", err)
+				return ip, port, ipType, err
+			}
+			ip = host
+			port = p
+		} else {
+			// 纯 IP，无端口
+			ip = ipStr
+			port = ""
+		}
 	}
-	if ip.To4() != nil {
-		return "IPv4"
+
+	logx.Infof("ip: %s, port: %s", ip, port)
+
+	// 解析 IP
+	netIp := net.ParseIP(strings.Trim(ip, "[]"))
+	if netIp == nil {
+		logx.Errorf("❌ 无效的 IP 地址")
+		return ip, port, ipType, errors.New("unknown ip format")
 	}
-	return "IPv6"
+	// 类型判断
+	if netIp.To4() != nil {
+		ipType = "IPv4"
+		return ip, port, ipType, nil
+	} else {
+		ipType = "IPv6"
+		return ip, port, ipType, nil
+	}
 }
