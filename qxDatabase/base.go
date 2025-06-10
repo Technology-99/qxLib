6e0eec78c7a4eb3b03ac008ee828f6427cd20edc
howard <howard.huang@itech99.com>
2025-06-10 15:18:33 +0800

    feat: 调整新版数据库基础模型

--- qxDatabase/base.go
+++ qxDatabase/base.go
@@ -5,6 +5,7 @@ import (
 	"time"
 )
 
+// note： 兼容实现
 type BaseModel struct {
 	ID        uint           `gorm:"primarykey" json:"id"`
 	CreatedAt time.Time      `gorm:"column:created_at;comment:创建时间;" json:"created_at"`
@@ -18,14 +19,44 @@ type UniqueIdAdminModel struct {
 	DeletedBy string `gorm:"index:idx_deleted_by;column:deleted_by;comment:删除者;type: varchar(255)" json:"-"`
 }
 
+type BaseTenantModel struct {
+	CreatedTenantBy string `gorm:"index:idx_created_tenant_by;column:created_tenant_by;comment:创建数据的租户;type: varchar(255)" json:"created_tenant_by"`
+	UpdatedTenantBy string `gorm:"index:idx_updated_tenant_by;column:updated_tenant_by;comment:更新数据的租户;type: varchar(255)" json:"updated_tenant_by"`
+	DeletedTenantBy string `gorm:"index:idx_deleted_tenant_by;column:deleted_tenant_by;comment:删除数据的租户;type: varchar(255)" json:"-"`
+}
+
 type BaseAdminModel struct {
 	CreatedBy uint `gorm:"column:created_by;comment:创建者;type: int" json:"created_by"`
 	UpdatedBy uint `gorm:"column:updated_by;comment:更新者;type: int" json:"updated_by"`
 	DeletedBy uint `gorm:"column:deleted_by;comment:删除者;type: int" json:"-"`
 }
 
-type BaseTenantModel struct {
-	CreatedTenantBy string `gorm:"index:idx_created_tenant_by;column:created_tenant_by;comment:创建数据的租户;type: varchar(255)" json:"created_tenant_by"`
-	UpdatedTenantBy string `gorm:"index:idx_updated_tenant_by;column:updated_tenant_by;comment:更新数据的租户;type: varchar(255)" json:"updated_tenant_by"`
-	DeletedTenantBy string `gorm:"index:idx_deleted_tenant_by;column:deleted_tenant_by;comment:删除数据的租户;type: varchar(255)" json:"-"`
+// note: 审计字段新版实现
+type AuditByStringModel struct {
+	CreatedBy string `gorm:"index:idx_created_by;column:created_by;comment:创建者;type: varchar(255)" json:"created_by"`
+	UpdatedBy string `gorm:"index:idx_updated_by;column:updated_by;comment:更新者;type: varchar(255)" json:"updated_by"`
+	DeletedBy string `gorm:"index:idx_deleted_by;column:deleted_by;comment:删除者;type: varchar(255)" json:"-"`
+}
+type AuditByUintModel struct {
+	CreatedBy uint `gorm:"index:idx_created_by;column:created_by;comment:创建者;type:int" json:"created_by"`
+	UpdatedBy uint `gorm:"index:idx_updated_by;column:updated_by;comment:更新者;type:int" json:"updated_by"`
+	DeletedBy uint `gorm:"index:idx_deleted_by;column:deleted_by;comment:删除者;type:int" json:"-"`
+}
+
+// note: rpc新版实现
+type BaseRpcModel struct {
+	ID        uint           `gorm:"primarykey" json:"id"`
+	CreatedAt time.Time      `gorm:"column:created_at;comment:创建时间;" json:"createdAt"`
+	UpdatedAt time.Time      `gorm:"column:updated_at;comment:更新时间;" json:"updatedAt"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
+}
+type AuditRpcByUintModel struct {
+	CreatedBy string `gorm:"index:idx_created_by;column:created_by;comment:创建者;type:int" json:"createdBy"`
+	UpdatedBy string `gorm:"index:idx_updated_by;column:updated_by;comment:更新者;type:int" json:"updatedBy"`
+	DeletedBy string `gorm:"index:idx_deleted_by;column:deleted_by;comment:删除者;type:int" json:"-"`
+}
+type AuditRpcByStringModel struct {
+	CreatedBy string `gorm:"index:idx_created_by;column:created_by;comment:创建者;type: varchar(255)" json:"createdBy"`
+	UpdatedBy string `gorm:"index:idx_updated_by;column:updated_by;comment:更新者;type: varchar(255)" json:"updatedBy"`
+	DeletedBy string `gorm:"index:idx_deleted_by;column:deleted_by;comment:删除者;type: varchar(255)" json:"-"`
 }
