3da52c52a85f750f0f8ccf85624fd6fd91719aaf
howard <howard.huang@itech99.com>
2025-06-13 15:26:28 +0800

    feat: 完成安全标准的跨域配置标准版

--- qxCors/cors.go
+++ qxCors/cors.go
@@ -1,93 +1,52 @@
 package qxCors
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"strings"
 )
 
 const (
-	allowOrigin      = "Access-Control-Allow-Origin"
-	allOrigins       = "*"
-	allowMethods     = "Access-Control-Allow-Methods"
-	allowHeaders     = "Access-Control-Allow-Headers"
-	allowCredentials = "Access-Control-Allow-Credentials"
-	exposeHeaders    = "Access-Control-Expose-Headers"
-	requestMethod    = "Access-Control-Request-Method"
-	requestHeaders   = "Access-Control-Request-Headers"
-	allowHeadersVal  = "Content-Type, Origin, X-CSRF-Token, Authorization, X-Request-ID, Host, X-Amz-Content-Sha256, X-Amz-Date"
-	exposeHeadersVal = "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers"
-	methods          = "GET, POST"
-	allowTrue        = "true"
-	maxAgeHeader     = "Access-Control-Max-Age"
-	maxAgeHeaderVal  = "86400"
-	varyHeader       = "Vary"
-	originHeader     = "Origin"
+	allowMethods  = "Access-Control-Allow-Methods"
+	allowHeaders  = "Access-Control-Allow-Headers"
+	exposeHeaders = "Access-Control-Expose-Headers"
+	maxAgeHeader  = "Access-Control-Max-Age"
 )
 
-func CustomNotAllowedHandler() func(http.ResponseWriter) {
-	return func(r http.ResponseWriter) {
-		r.Header().Set(allowHeaders, allowHeadersVal)
-	}
+type Cors struct {
+	AllowedOrigins  []string `json:",default=['http://localhost:8888']"`
+	AllowMethods    []string `json:",default=['HEAD','GET','POST']"`
+	AllowHeaders    []string `json:",default=['Content-Type','Origin','X-CSRF-Token','Authorization','X-Request-ID','Host','X-Amz-Content-Sha256','X-Amz-Date','Range']"`
+	ExposeHeaders   []string `json:",default=['Content-Length','Access-Control-Allow-Origin','Access-Control-Allow-Headers']"`
+	MaxAgeHeaderVal int64    `json:",default=86400"`
 }
 
-func CustomMiddleware() func(w http.Header) {
-	return func(w http.Header) {
-		w.Set(allowHeaders, allowHeadersVal)
-	}
-}
+func CustomNotAllowedFn() func(http.ResponseWriter) {
+	return func(w http.ResponseWriter) {
+		// note: 请求了没注册的方法
+		// 可选：写入你希望暴露的 CORS 响应头
+		w.Header().Set("Content-Type", "application/json")
 
-func checkAndSetHeaders(w http.ResponseWriter, r *http.Request, origins []string) {
-	setVaryHeaders(w, r)
-
-	if len(origins) == 0 {
-		setHeader(w, allOrigins)
-		return
-	}
-
-	origin := r.Header.Get(originHeader)
-	if isOriginAllowed(origins, origin) {
-		setHeader(w, origin)
-	}
-}
-
-func isOriginAllowed(allows []string, origin string) bool {
-	origin = strings.ToLower(origin)
-
-	for _, allow := range allows {
-		if allow == allOrigins {
-			return true
+		// 可选：自定义错误响应体
+		resp := map[string]string{
+			"error":   "Method Not Allowed",
+			"message": "The requested method is not allowed on this endpoint.",
 		}
 
-		allow = strings.ToLower(allow)
-		if origin == allow {
-			return true
-		}
-
-		if strings.HasSuffix(origin, "."+allow) {
-			return true
-		}
+		w.WriteHeader(http.StatusMethodNotAllowed) // 405
+		_ = json.NewEncoder(w).Encode(resp)
 	}
-
-	return false
 }
 
-func setHeader(w http.ResponseWriter, origin string) {
-	header := w.Header()
-	header.Set(allowOrigin, origin)
-	header.Set(allowMethods, methods)
-	header.Set(allowHeaders, allowHeadersVal)
-	header.Set(exposeHeaders, exposeHeadersVal)
-	if origin != allOrigins {
-		header.Set(allowCredentials, allowTrue)
-	}
-	header.Set(maxAgeHeader, maxAgeHeaderVal)
-}
-
-func setVaryHeaders(w http.ResponseWriter, r *http.Request) {
-	header := w.Header()
-	header.Add(varyHeader, originHeader)
-	if r.Method == http.MethodOptions {
-		header.Add(varyHeader, requestMethod)
-		header.Add(varyHeader, requestHeaders)
+func CustomMiddlewareFn(allowMethodsInput []string, allowHeadersInput []string, exposeHeadersInput []string, maxAgeHeaderVal int64) func(header http.Header) {
+	return func(header http.Header) {
+		tempMethods := strings.Join(allowMethodsInput, ",")
+		header.Set(allowMethods, tempMethods)
+		tempAllowHeaders := strings.Join(allowHeadersInput, ",")
+		header.Set(allowHeaders, tempAllowHeaders)
+		tempExposeHeaders := strings.Join(exposeHeadersInput, ",")
+		header.Set(exposeHeaders, tempExposeHeaders)
+		header.Set(maxAgeHeader, fmt.Sprintf("%d", maxAgeHeaderVal))
 	}
 }
