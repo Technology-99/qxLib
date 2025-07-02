d66b83f16455c4e4519ae1d7d82c965890545373
howard <howard.huang@itech99.com>
2025-07-02 14:14:39 +0800

    feat: 更新短链id的生成方式

--- qxShortId/qxShortId.go
+++ qxShortId/qxShortId.go
@@ -25,11 +25,24 @@ func base62EncodeBigInt(n *big.Int) string {
 	return result
 }
 
-// 哈希+编码函数
+// note: 保持向下兼容，以前的 GenerateShortID 函数, 使用ShortId替代
+// Deprecated: Use NewFunction instead.
 func GenerateShortID(biz string, mid int64, salt string) string {
 	// 拼接数据
 	raw := fmt.Sprintf("%s_%d_%s", biz, mid, salt)
 
+	// 哈希
+	hash := sha256.Sum256([]byte(raw))
+
+	// 取前 8-16 字节转换为整数（防止过长）
+	hashSlice := hash[:12] // 可调整长度
+	bigInt := new(big.Int).SetBytes(hashSlice)
+
+	// 编码为 base62
+	return base62EncodeBigInt(bigInt)
+}
+
+func ShortId(raw string) string {
 	// 哈希
 	hash := sha256.Sum256([]byte(raw))
 
